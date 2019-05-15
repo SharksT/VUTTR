38ed4347ace56e9d55a45edc807b9a72f2644c33
Felipe <felipekaires@hotmail.com>
2019-05-15 19:50:24 -0300

    Tag

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 const ToolsController = require("./controllers/toolsController");
 
 routes.get("/tools", ToolsController.show);
-routes.get("/tag/:id", ToolsController.showTag);
+routes.get("/tools", ToolsController.showTag);
 routes.post("/tools", ToolsController.store);
 routes.post("/tools/:id", ToolsController.delete);
 module.exports = routes;
